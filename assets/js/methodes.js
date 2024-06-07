69c078fa2e483df761c153ed2cc3ddbe0d56be1d
erdaserhan <erhan209@gmail.com>
2024-06-07 11:09:46 +0200

    orientation page updated

---
+++ assets/js/methodes.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    //Continuer à lire bouton
+    $("#orientationContinue").hide();
+    $("#buttonStopContinue").hide();
+    $("#buttonContinue").click(function () {
+      $("#orientationContinue").slideDown("slow");
+      $("#buttonContinue").hide(1000);
+      $("#buttonStopContinue").show(1000);
+    });
+    $("#buttonStopContinue").click(function(){
+      $("#orientationContinue").slideUp("slow");
+      $("#buttonStopContinue").hide(1000);
+      $("#buttonContinue").show(2000);
+    });    
+  });
