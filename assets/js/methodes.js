46f28316d43f8ee56eb9661a3f67c0ce5ea735f9
erdaserhan <erhan209@gmail.com>
2024-06-09 17:39:09 +0200

    methodes updated

--- assets/js/methodes.js
+++ assets/js/methodes.js
@@ -1,16 +1,39 @@
 $(document).ready(function(){
     //Continuer à lire bouton
-    $("#orientationContinue").hide();
-    $("#buttonStopContinue").hide();
-    $("#buttonContinue").click(function () {
-      $("#orientationContinue").slideDown("slow");
-      $("#buttonContinue").hide(1000);
-      $("#buttonStopContinue").show(1000);
+    $(".orientationContinue1").hide();
+    $(".buttonStopContinue1").hide();
+    $(".buttonContinue1").click(function () {
+      $(".orientationContinue1").slideDown("slow");
+      $(".buttonContinue1").hide(1000);
+      $(".buttonStopContinue1").show(1000);
     });
-    $("#buttonStopContinue").click(function(){
-      $("#orientationContinue").slideUp("slow");
-      $("#buttonStopContinue").hide(1000);
-      $("#buttonContinue").show(2000);
+    $(".buttonStopContinue1").click(function(){
+      $(".orientationContinue1").slideUp("slow");
+      $(".buttonStopContinue1").hide(1000);
+      $(".buttonContinue1").show(2000);
     });  
-    $(".photoAccom").fadeOut();
+    $(".orientationContinue2").hide();
+    $(".buttonStopContinue2").hide();
+    $(".buttonContinue2").click(function () {
+      $(".orientationContinue2").slideDown("slow");
+      $(".buttonContinue2").hide(1000);
+      $(".buttonStopContinue2").show(1000);
+    });
+    $(".buttonStopContinue2").click(function(){
+      $(".orientationContinue2").slideUp("slow");
+      $(".buttonStopContinue2").hide(1000);
+      $(".buttonContinue2").show(2000);
+    }); 
+    $(".orientationContinue3").hide();
+    $(".buttonStopContinue3").hide();
+    $(".buttonContinue3").click(function () {
+      $(".orientationContinue3").slideDown("slow");
+      $(".buttonContinue3").hide(1000);
+      $(".buttonStopContinue3").show(1000);
+    });
+    $(".buttonStopContinue3").click(function(){
+      $(".orientationContinue3").slideUp("slow");
+      $(".buttonStopContinue3").hide(1000);
+      $(".buttonContinue3").show(2000);
+    }); 
   });
